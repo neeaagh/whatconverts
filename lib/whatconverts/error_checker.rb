1fd4537fbe05dc3fde505613e7c643b89fc72ee4
Joe Macbook Pro <neeaagh@gmail.com>
2016-08-03 15:05:20 -0400

    implement error_checker

--- lib/whatconverts/error_checker.rb
+++ lib/whatconverts/error_checker.rb
@@ -1,5 +1,24 @@
 module Whatconverts
   class ErrorChecker
+    
+    def initialize(api_response)
+      @api_response = api_response
+    end
 
+    def error_if_appropriate
+      return ApiError.new(response_body) if is_error?
+    end
+
+    private
+
+    def is_error?
+      @api_response.status.to_i >= 400
+    end
+
+    def response_body
+      JSON.parse(@api_response.body)['error_message']
+    rescue
+      @api_response.body
+    end
   end
 end
