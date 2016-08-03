0de6016fd276f1d845b9c2bf3786dd633cebae47
Joe Macbook Pro <neeaagh@gmail.com>
2016-08-03 15:01:15 -0400

    implemented client http service

--- lib/whatconverts/client.rb
+++ lib/whatconverts/client.rb
@@ -7,5 +7,21 @@ module Whatconverts
     def initialize
       yield self if block_given?
     end
+
+    def http_service(endpoint, params = {})
+      method = params.delete(:method) { :get }
+      response =  connection.send(method, endpoint, params)
+      error = ErrorChecker.new(response).error_if_appropriate
+      raise error if error
+      JOSN.parse(response.body)
+    end
+
+    private
+
+    def connection
+      conn = Faraday.new(url: API_URL)
+      conn.basic_auth(api_token, api_secret)
+      conn
+    end
   end
 end
