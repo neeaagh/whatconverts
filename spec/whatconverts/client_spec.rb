2228609af678f69693e86b50550f8d95a6b09a5e
Joe Macbook Pro <neeaagh@gmail.com>
2016-09-23 11:04:46 -0400

    added account fixtures

--- spec/whatconverts/client_spec.rb
+++ spec/whatconverts/client_spec.rb
@@ -64,4 +64,25 @@ describe Whatconverts::Client do
       expect(lead_response['lead_id']).to eq(lead_id)
     end
   end
+
+  describe '#accounts' do
+
+    it 'returns all accounts' do
+      stub_get('accounts').to_return(body: fixture('accounts_all.json'))
+      
+      accounts = @client.accounts
+      expect(a_get('accounts')).to have_been_made
+      expect(accounts['accounts'].count).to eq(3)
+    end
+
+    it 'returns filtered accounts' do
+      stub_get('accounts', { accounts_per_page: 1 }).to_return(body: fixture('accounts_filtered.json'))
+
+      accounts = @client.accounts(accounts_per_page: 1)
+      expect(a_get('accounts', accounts_per_page: 1)).to have_been_made
+      expect(accounts['accounts'].count).to eq(1)
+      expect(accounts['accounts'].first['account_name']).to eq('Auto Store')
+    end
+  end
+
 end
