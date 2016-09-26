850ae6a20a72985b4ee5353f5df0fedf171ff621
Joe Macbook Pro <neeaagh@gmail.com>
2016-09-26 14:07:49 -0400

    add params to profile resource. profile specs.

--- lib/whatconverts/client/profile.rb
+++ lib/whatconverts/client/profile.rb
@@ -1,27 +1,27 @@
 module Whatconverts
   class Client
     module Profile
-      def profiles(account_id)
-        http_service.make_request("accounts/#{acount_id}/profiles")
+      def profiles(account_id, params = {})
+        http_service.make_request("accounts/#{account_id}/profiles", params)
       end
 
-      def profile(account_id, profile_id)
-        http_service.make_request("accounts/#{account_id}/profiles/#{profile_id}")
+      def profile(account_id, profile_id, params = {})
+        http_service.make_request("accounts/#{account_id}/profiles/#{profile_id}", params)
       end
 
-      def create_profile(account_id)
+      def create_profile(account_id, params = {})
         params.merge!(method: :post)
-        http_service.make_request("accounts/#{account_id}/profiles")
+        http_service.make_request("accounts/#{account_id}/profiles", params)
       end
 
-      def edit_profile(account_id, profile_id)
+      def edit_profile(account_id, profile_id, params = {})
         params.merge!(method: :post)
-        http_service.make_request("accounts/#{account_id}/profiles/#{profile_id}")
+        http_service.make_request("accounts/#{account_id}/profiles/#{profile_id}", params)
       end
 
-      def delete_profile(account_id, profile_id)
+      def delete_profile(account_id, profile_id, params = {})
         params.merge!(method: :delete)
-        http_service.make_request("accounts/#{account_id}/profiles/#{profile_id}")
+        http_service.make_request("accounts/#{account_id}/profiles/#{profile_id}", params)
       end
 
     end
--- spec/whatconverts/client/profile_spec.rb
+++ spec/whatconverts/client/profile_spec.rb
@@ -6,10 +6,82 @@ describe Whatconverts::Client::Profile do
       client.api_token = 'API TOKEN'
       client.api_secret = 'API SECRET'
     end
+    @account_id = 123
+    @profiles_endpoint = "accounts/#{@account_id}/profiles"
   end
 
   describe '#profiles' do
 
+    it 'returns all profiles' do
+      stub_get(@profiles_endpoint).to_return(body: fixture('profiles_all.json'))
+
+      profiles = @client.profiles(123)
+      expect(a_get(@profiles_endpoint)).to have_been_made
+      expect(profiles['profiles'].count).to eq(3)
+    end
+
+    it 'returns filtered profiles' do
+      stub_get(@profiles_endpoint, { profiles_per_page: 1 }).to_return(body: fixture('profiles_filtered.json'))
+
+      profiles = @client.profiles(@account_id, profiles_per_page: 1)
+      expect(a_get(@profiles_endpoint, profiles_per_page: 1)).to have_been_made
+      expect(profiles['profiles'].count).to eq(1)
+      expect(profiles['profiles_per_page']).to eq(1)
+    end
+  end
+
+  describe '#profile' do
+
+    it 'returns the correct profile' do
+      profile_id = 33333
+      stub_get("#{@profiles_endpoint}/#{profile_id}").to_return(body: fixture('profile.json'))
+      profile = @client.profile(@account_id, profile_id)
+      expect(a_get("#{@profiles_endpoint}/#{profile_id}")).to have_been_made
+      expect(profile['profiles'].first['profile_id']).to eq(profile_id)
+    end
+  end
+
+  describe '#create_profile' do
+
+    it 'creates a profile' do
+      params = {
+        profile_name: 'Test Profile'
+      }
+      stub_post(@profiles_endpoint, params)
+        .to_return(
+          body: '{"profile_id":123, "profile_name":"Test Profile", "date_created": "2016-06-24T17:24:42Z"}'
+      )
+      response = @client.create_profile(@account_id, params)
+      expect(a_post(@profiles_endpoint, params)).to have_been_made
+      expect(response['profile_id']).to eq(123)
+      expect(response['profile_name']).to eq('Test Profile')
+    end
+  end
+
+  describe '#edit_profile' do
+
+    it 'updates an profile' do
+      params = { profile_name: 'Test Profile 2' }
+      profile_id = 123
+      stub_post("#{@profiles_endpoint}/#{profile_id}", params)
+        .to_return(
+          body: '{"profile_id": 123, "profile_name":"Test Account 2"}, "date_created": "2016-06-24T17:24:42Z"}'
+      )
+      response = @client.edit_profile(@account_id, profile_id, params)
+      expect(a_post("#{@profiles_endpoint}/#{profile_id}", params)).to have_been_made
+      expect(response['profile_name']).to eq('Test Account 2')
+    end
+  end
+
+  describe '#delete_profile' do
+
+    it ' deletes an profile' do
+      profile_id = 123
+      stub_delete("#{@profiles_endpoint}/#{profile_id}").to_return(body: '{"profile_id":123}')
+      response = @client.delete_profile(@account_id, profile_id)
+      expect(a_delete("#{@profiles_endpoint}/#{profile_id}")).to have_been_made
+      expect(response['profile_id']).to eq(profile_id)      
+    end
   end
 
 end
